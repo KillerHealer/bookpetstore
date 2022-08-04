4b4b81568671b78837ad0fcc70823b18686124ae
KillerHealer <noam142196@gmail.com>
2022-08-04 12:14:19 +0300

    second set of tests and some logging for bookstore

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-import json
 import pytest
 import logging
 import requests
 
-
+logger = logging.Logger
 url = "https://bookstore.toolsqa.com/"
 header = {'accept': 'application/json'}
 my_book = {"isbn": 1, "title": "my_book", "subTitle": "my life", "author": "me", "publish_date": "1.1.2000",
@@ -17,16 +16,31 @@ login = {"userName": "string", "password": "String123@"}
 
 
 def test_post_login_account():
+    """
+    tries to log in the user called 'login' and sees if it works
+    :return:
+    """
+    logging.info("trying to login 'string'")
     response = requests.post(f"{url}Account/v1/Authorized", data=login, headers=header)
     assert response.status_code == 200
 
 
 def test_post_login_account_generate_token():
+    """
+    tries to get an authorized token for said account
+    :return:
+    """
+    logging.info("trying to get a token")
     response = requests.post(f"{url}Account/v1/GenerateToken", data=login, headers=header)
     assert response.status_code == 200
 
 
 def test_post_new_user():
+    """
+    tries to create a new user and then tries to delete it for ease of use
+    :return:
+    """
+    logging.info("trying to create a new user")
     response = requests.post(f"{url}Account/v1/User", data=my_user, headers=header)
     assert response.status_code == 201
     ddid = ""
@@ -36,5 +50,19 @@ def test_post_new_user():
         if char == ",":
             break
         ddid += char
+    logging.info("trying to delete the user i just created")
     response2 = requests.delete(f"{url}Account/v1/User/{ddid}", headers=header)
     assert response2.status_code == 200
+
+
+def test_delete_user():
+    """
+    tries to delete a user
+    :return:
+    """
+    logging.info("trying to delete a user with a 30 character userID")
+    ddid = "ca6fea99-bc81-4f06-9ada-14edb56cada1"
+    response = requests.delete(f"{url}Account/v1/User/{ddid}", headers=header)
+    assert response.status_code == 200
+
+
