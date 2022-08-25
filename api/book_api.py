df7cd53dff904a2d6e45d422d692dfeb21dce811
KillerHealer <noam142196@gmail.com>
2022-08-26 01:37:29 +0300

    started on book_api.py

--- api/book_api.py
+++ api/book_api.py
@@ -9,3 +9,29 @@ class BookApi:
         self._session = requests.session()
         self._session.headers.update(self._headers)
 
+    def get_books(self):
+        res = self._session.get(f"{self._url}s", headers=self._headers)
+        if res.status_code != 200:
+            mm = res.json()
+            my_mm = MessageModel(**mm)
+            return my_mm
+        else:
+            return dict(res)
+
+    def post_books(self):
+        res = self._session.post(f"{self._url}s", headers=self._headers)
+        if res.status_code != 200:
+            mm = res.json()
+            my_mm = MessageModel(**mm)
+            return my_mm
+        else:
+            return dict(res)
+
+    def delete_books(self):
+        res = self._session.delete(f"{self._url}s", headers=self._headers)
+        if res.status_code != 200:
+            mm = res.json()
+            my_mm = MessageModel(**mm)
+            return my_mm
+        else:
+            return dict(res)
